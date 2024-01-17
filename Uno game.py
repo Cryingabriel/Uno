564deba585dd7e4c8c4703a2e20973ccede2d470
Cryingabriel <Cai19mac@gmail.com>
2024-01-17 15:05:08 -0700

    lo

--- Uno game.py
+++ Uno game.py
@@ -1,6 +1,36 @@
-#42
-class card:
-    def __init__(self,number,color):
-        self.num = number
-        self.color = color
-cards = [card(i,"r") for i in range(10)]
+import pygame
+from pygame.math import Vector2
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((800,800))
+mousepos = (0,0)
+
+playerPos = Vector2(mousepos[0], mousepos[1])
+mexit = False
+timer = 30
+
+
+while not mexit:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mexit =  True
+    
+    if timer == 0:
+        pass
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    #Render Section
+    screen.fill((0,0,0))
+
+pygame.quit()
