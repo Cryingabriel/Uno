0545305bc486d1f59d20f62183962f33f9a740d8
Cryingabriel <Cai19mac@gmail.com>
2024-01-24 15:40:07 -0700

    plpl

--- Players.py
+++ Players.py
@@ -1,21 +1,32 @@
 import pygame
 import math
 from pygame.math import Vector2
-from Uno import screen
 from cards import card
 
+screen = pygame.display.set_mode((800,800))
 class Player():
     def __init__(self, xpos, ypos):
         self.pos = Vector2(xpos, ypos)
         self.cards = []
     
     def draw(self):
-        pygame.draw.rect(screen, (200,200,200), (80,80,80,80))
+        pygame.draw.rect(screen, (200,200,200), (self.pos.x, self.pos.y,80,80))
 
     
     def inventory(self):
         pass
 
-    def move(self, mousex, mousey):
-        self.pos.x = mousex
-        self.pos.y = mousey
+    def move(self, mouse):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                mouse = event.pos
+                if mouse[0] > self.pos.x:
+                    self.pos.x = mouse[0] - 40
+                elif mouse[0] < self.pos.x:
+                    self.pos.x = mouse[0] -40
+                
+                if mouse[1] > self.pos.y:
+                    self.pos.y = mouse[1] -40
+                elif mouse[1] < self.pos.y:
+                    self.pos.y = mouse[1] - 40
+        
--- Uno.py
+++ Uno.py
@@ -1,10 +1,13 @@
 import pygame
 from pygame.math import Vector2
-screen = pygame.display.set_mode((800,800))
 import Players
 from Players import Player
+from Players import screen
 clock = pygame.time.Clock()
-mousepos = (0,0)
+
+xpos = 0
+ypos = 0
+mousepos = (xpos,ypos)
 
 playerPos = Vector2(400,400)
 mexit = False
@@ -14,7 +17,7 @@ timer = 30
 player1 = Player(playerPos.x, playerPos.y)
 
 while not mexit:
-    clock.tick(60)
+    #clock.tick(60)
 
     for event in pygame.event.get(): #Event queue
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -29,7 +32,9 @@ while not mexit:
     #Render Section
     screen.fill((2,20,200))
 
-    player1.move(playerPos.x, playerPos.y)
+    player1.draw()
+
+    player1.move(mousepos)
 
     pygame.display.flip()
 
